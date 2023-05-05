edfddce07d18be358ffc5987d57e579bf206ff3f
Vlad <69871983+nicelir1996@users.noreply.github.com>
2023-05-05 07:15:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-python scripts/check_requirements.py requirements.txt
-if [ $? -eq 1 ]
-then
-    echo Installing missing packages...
-    pip install -r requirements.txt
-fi
+echo Installing missing packages...
+pip install -r requirements.txt
 python -m swarmai.__main__
 read -p "Press any key to continue..."
